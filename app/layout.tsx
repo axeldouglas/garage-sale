20db54faab6e157edd43ea5a11d5a9ba5dc2e3f7
Axel Santos <axel@fullstacklabs.co>
2026-06-10 10:56:24 -0300

    chore: env production

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL ?? ''),
+  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL ?? 'https://garage-sale-iota.vercel.app/'),
   title: 'Liquidação',
   description: 'Móveis, eletrônicos e muito mais.',
 };
